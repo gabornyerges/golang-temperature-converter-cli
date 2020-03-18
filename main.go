2018226be1b301945b5690138f2f9c2be67b66bc
Gabor Nyerges <gabor@nyerg.es>
2020-03-18 08:26:29 +0100

    Completed module

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
+  "errors"
+  "fmt"
+  "os"
   "strings"
 )
 
@@ -23,29 +23,42 @@ func main() {
   }
   originUnit = strings.ToUpper(os.Args[1])
 
-	for {
-		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+  for {
+    fmt.Print("What is the current temperature in " + originUnit + " ? ")
+    _, err = fmt.Scanln(&originValue)
+    if err != nil {
+      printError(errReadingInput)
+    }
+    if originUnit == "C" {
+      convertToFahrenheit(originValue)
+    } else {
+      convertToCelsius(originValue)
+    }
 
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+    fmt.Print("Would you like to convert another temperature ? (y/n) ")
+    _, err = fmt.Scanln(&shouldConvertAgain)
+    if err != nil {
+      printError(errReadingInput)
+    }
 
-		if shouldConvertAgain != "Y" {
-			fmt.Println("Good bye!")
-			break
-		}
-	}
+    if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
+      fmt.Println("Good bye!")
+      break
+    }
+  }
 }
 
 func printError(err error) {
-	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	os.Exit(1)
+  fmt.Fprintf(os.Stderr, "error: %v\n", err)
+  os.Exit(1)
 }
 
 func convertToCelsius(value float64) {
-	convertedValue := (value - 32) * 5 / 9
-	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
+  convertedValue := (value - 32) * 5 / 9
+  fmt.Printf("%v F = %.0f C\n", value, convertedValue)
 }
 
 func convertToFahrenheit(value float64) {
-	convertedValue := (value * 9 / 5) + 32
-	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+  convertedValue := (value * 9 / 5) + 32
+  fmt.Printf("%v C = %.0f F\n", value, convertedValue)
 }
